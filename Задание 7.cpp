909d6f5c9c7eb06f4c2d8d04fbdc7e783698e69e
Sergey-Nenahov <70951479+Sergey-Nenahov@users.noreply.github.com>
2020-10-12 21:25:38 +0300

    готовый вариант

---
+++ Задание 7.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+	int b = 6;
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	cout << "----------------------------" << endl;
+	_asm
+	{
+		xor eax, eax
+		xor ebx, ebx
+		mov         eax, dword ptr[a]
+		mov         ebx, dword ptr[b]
+		XCHG eax, ebx
+		mov          dword ptr[a], eax
+		mov          dword ptr[b], ebx
+
+	}
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	system("pause");
+	return 0;
+}
